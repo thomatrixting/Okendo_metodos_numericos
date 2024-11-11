90feb623025f0ca30f05c6435b23d427588690a2
thomas <thomas.quamtum@gmail.com  git config --global user.name thomas>
2024-11-11 17:53:36 -0500

    cleaned much more and added comments and corrected and error of the printing decimal places

--- aprox_pi.cpp
+++ aprox_pi.cpp
@@ -1,29 +1,30 @@
+//standar c++ 17
 #include <iostream>
 #include <cmath>
-#include <string>
 
 # define M_PIl 3.141592653589793238462643383279502884L //difine pi as a long double with 18 decimal places
 
 using std::cout;
 using std::cin;
 
-const unsigned int PRECISION = 18; // Max presicion corresponding a long double
+const unsigned int PRECISION = 18; // Max presicion corresponding a long double in case of ussing just double to safe memory the presicion would be of 16
 
 long double pi_aprox(unsigned int n);
 long double compute_error(long double computed_value, long double true_value);
 
 int main() {
 
-    cout.precision(PRECISION);
     cout.setf(std::ios::scientific);
     
-    for (int n = 0; n <= 20; n++) {
+    for (int n = 1; n <= 20; n++) {
+
         long double pi_estimation = pi_aprox(n);
         long double error = compute_error(pi_estimation, M_PIl);
 
         cout << "pi estimation for n: " << n << " ";
 
         // Print pi_estimation in scientific notation with the given decimal places
+        cout.precision(PRECISION);
         cout << pi_estimation << " ";
 
         // Print error with 8 decimal places 
@@ -31,12 +32,14 @@ int main() {
         cout << "error: " << error * 100 << "%\n";
     }
     
-    return 0;
+    return 0; 
 }
 
 long double pi_aprox(unsigned int n) {
+
     long double pi_estimation = 0;
     long double instance_value = 0;
+
     for (unsigned int k = 0; k <= n; k++) { //depends on the procesor but adding the values this ways reduces the error but takes more memory
         long double f1 = 4.0 / (8.0 * k + 1.0); 
         long double f2 = 2.0 / (8.0 * k + 4.0);
@@ -54,7 +57,8 @@ long double pi_aprox(unsigned int n) {
 
 long double compute_error(long double computed_value, long double true_value) {
 
-    long double error = std::abs(computed_value - true_value) / true_value;
+    long double error = std::abs(computed_value - true_value) / true_value; //compute the relative error
+    //the error will get stuck at the max presicion as each new sum from this point cannot be represented accurally so it cannot inpact meaningfully the computing
 
     return error;
 }
