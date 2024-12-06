9f5ff245fb26f9a62a0e0758149c38a5d2c74950
thomas <thomas.quamtum@gmail.com>
2024-12-06 08:47:59 -0500

    implemented gauss 5 and solved some errors

--- clase_10/gausian_integracion.cpp
+++ clase_10/gausian_integracion.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using std::cout;
 using std::cerr;
+using std::sqrt;
+using std::vector;
 
 using fptr = long double(long double); //function pointer
 
@@ -11,6 +14,7 @@ long double compute_error(long double true_value, long double estimated_value);
 long double define_integral(void);
 
 long double gauss2(fptr fun, long double a, long double b);
+long double gauss5(fptr fun, long double a, long double b);
 
 int main(int argc, char **argv){
 
@@ -30,13 +34,19 @@ int main(int argc, char **argv){
     double a = std::stold(argv[1]);
     double b = std::stold(argv[2]);
 
-    long double integral_value = gauss2(function, a, b);
+    long double integral_value_2 = gauss2(function, a, b);
+    long double integral_value_5 = gauss5(function, a, b);
+
+
     long double real_value = define_integral(); //esta definido por 0 a 1
+    long double error_2 = compute_error(real_value,integral_value_2);
+    long double error_5 = compute_error(real_value,integral_value_5);
 
-    long double error = compute_error(real_value,integral_value);
+    cout <<  "integral 2: "<< integral_value_2 << "\n";
+    cout << "error for the integrla between 0 and 1: " << error_2 << "\n";
 
-    cout <<  "integral: "<< integral_value << "\n";
-    cout << "error for the integrla between 0 and 1: " << error << "\n";
+    cout <<  "integral 5: "<< integral_value_5 << "\n";
+    cout << "error for the integrla between 0 and 1: " << error_5 << "\n";
     
 }
 
@@ -64,11 +74,45 @@ long double gauss2(fptr fun, long double a, long double b){
     for(int i=0; i <= 1; i++){
 
         sum = sum + fun(
-            ((b-a)/2.0)*X[i] + ((b-a)/2.0)
+            ((b-a)/2.0)*X[i] + ((b+a)/2.0)
         );
 
     }
 
     return ((b-a)/2.0)*sum;
 
+}
+
+long double gauss5(fptr fun, long double a, long double b){
+    
+    vector<long double> X = {
+        - (1./3.)*( sqrt( 5. - 2* ( sqrt(10./7.) ) ) ) ,         
+          (1./3.)*( sqrt( 5. - 2* ( sqrt(10./7.) ) ) ) , 
+        - (1./3.)*( sqrt( 5. + 2* ( sqrt(10./7.) ) ) ) , 
+          (1./3.)*( sqrt( 5. + 2* ( sqrt(10./7.) ) ) ) , 
+         0
+    };
+    
+    vector<long double> W = {
+        (322. + 13.* sqrt(70))/900. ,
+        (322. + 13.* sqrt(70))/900. ,
+        (322. - 13.* sqrt(70))/900. ,
+        (322. - 13.* sqrt(70))/900. ,
+        128./225.
+    };
+
+    long double sum = 0;
+
+    const long double aux1 = (a+b)/2.;
+    const long double aux2 = (b-a)/2.;
+
+    for(int i=0; i <= X.size()-1; i++){
+
+        sum = sum + W[i]*fun(
+            (aux2)*X[i] + (aux1)
+        );
+
+    }
+
+    return (aux1)*sum;
 }
